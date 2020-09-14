956ae36337b03f12b1ffa62a0342d1eceaa7c4c0
Mattia Roccoberton <mat@blocknot.es>
2020-09-14 08:38:12 +0200

    WIP

--- app/assets/javascripts/activeadmin/dynamic_fields.js
+++ app/assets/javascripts/activeadmin/dynamic_fields.js
@@ -1,6 +1,33 @@
 (function () {
   'use strict'
 
+  const ACTIONS = {
+    addClass: (el, name) => el.addClass(name),
+    callback: (el, name) => {
+      if (window[name]) window[name](el.data('args'))
+      else console.log(`Warning - activeadmin_dynamic_fields: ${name}() not available [2]`)
+    },
+    fade: el => el.fadeIn(),
+    hide: el => el.hide(),
+    setValue: (el, value) => dfSetValue(el, value),
+    slide: el => el.slideDown()
+  }
+
+  const CONDITIONS = {
+    blank: el => el.val().length === 0 || !el.val().trim(),
+    checked: el => el.is(':checked'),
+    eq: (el, value) => el.val() == value,
+    not: (el, value) => el.val() != value,
+    not_checked: el => !el.is(':checked')
+  }
+
+  const REVERSE_ACTIONS = {
+    addClass: (el, name) => el.removeClass(name),
+    fade: el => el.fadeOut(),
+    hide: el => el.show(),
+    slide: el => el.slideUp()
+  }
+
   // Evaluate a condition
   function dfEvalCondition(el, args, on_change) {
     if (args.fn) {
@@ -32,7 +59,6 @@
     return undefined;
   }
 
-  // Prepare a field
   function dfSetupField(el) {
     let action = el.data('then') || el.data('action');
     let target, args = {};
@@ -104,9 +130,9 @@
 
   // Set the value of an element
   function dfSetValue(el, val) {
-    if (el.attr('type') != 'checkbox') el.val(val);
-    else el.prop('checked', val == '1');
-    el.trigger('change');
+    if (el.attr('type') == 'checkbox') el.prop('checked', val == '1')
+    else el.val(val)
+    el.trigger('change')
   }
 
   // Inline update - must be called binded on the editing element
@@ -161,18 +187,70 @@
     }
   }
 
+
+  function evalCondition(el) {
+    let condition = CONDITIONS[el.data('if')]
+    let condition_arg
+
+    if(!condition && el.data('eq')) {
+      condition = CONDITIONS['eq']
+      condition_arg = el.data('eq')
+    }
+    if(!condition && el.data('not')) {
+      condition = CONDITIONS['not']
+      condition_arg = el.data('not')
+    }
+    if(!condition && el.data('function')) condition = window[el.data('function')]
+
+    return [condition, condition_arg]
+  }
+
+  function initField(el) {
+    const [condition, condition_arg] = evalCondition(el)
+    const action_name = (el.data('then') || el.data('action')).substr(0, 8)
+    const action = ACTIONS[action_name]
+    const arg = (el.data('then') || el.data('action')).substr(9)
+    const reverse_action = REVERSE_ACTIONS[el.data('then') || el.data('action')]
+    if (typeof condition === 'undefined' || typeof action === 'undefined') return
+
+    // closest find for has many associations
+    let target
+    if (el.data('target')) target = el.closest('fieldset').find(el.data('target'))
+    else if (el.data('gtarget')) target = $(el.data('gtarget'))
+
+    if (condition(el, condition_arg)) action(target, arg)
+    else if (reverse_action) reverse_action(target, arg)
+
+    el.on('change', () => {
+      if (condition(el, condition_arg)) action(target, arg)
+      else if (reverse_action) reverse_action(target, arg)
+    })
+  }
+
   // Init
   $(document).ready(function () {
-    // Setup dynamic fields
-    $('.active_admin .input [data-if], .active_admin .input [data-function], .active_admin .input [data-eq], .active_admin .input [data-not]').each(function () {
-      dfSetupField($(this));
-    });
-    // Setup dynamic fields for has many associations
-    $('.active_admin .has_many_container').on('has_many_add:after', function (e, fieldset, container) {
-      $('.active_admin .input [data-if], .active_admin .input [data-function], .active_admin .input [data-eq], .active_admin .input [data-not]').each(function () {
-        dfSetupField($(this));
-      });
-    });
+    const selectors = '.active_admin .input [data-if], .active_admin .input [data-eq], .active_admin .input [data-not], .active_admin .input [data-function]'
+    $(selectors).each(function () {
+      initField($(this))
+    })
+    // Setup dynamic fields for associations
+    $('.active_admin .has_many_container').on('has_many_add:after', () => {
+      $(selectors).each(function () {
+        initField($(this))
+      })  
+    })
+
+    // // Setup dynamic fields
+    // $('.active_admin .input [data-if], .active_admin .input [data-function], .active_admin .input [data-eq], .active_admin .input [data-not]').each(function () {
+    //   dfSetupField($(this));
+    // });
+    // // Setup dynamic fields for has many associations
+    // $('.active_admin .has_many_container').on('has_many_add:after', function (e, fieldset, container) {
+    //   $('.active_admin .input [data-if], .active_admin .input [data-function], .active_admin .input [data-eq], .active_admin .input [data-not]').each(function () {
+    //     dfSetupField($(this));
+    //   });
+    // });
+
     // Set dialog icon link
     $('.active_admin [data-df-icon]').each(function () {
       $(this).append(' &raquo;');  // ' &bullet;'
